d5bde1bad548031fbfce46dcda1650b356f7dc9f
Anne Wilson <annewilson2591@gmail.com>
2019-04-28 23:47:02 -0600

    structure basics

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
+import images from "./images.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { images }
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Image List</h1>
+        {this.state.images.map(image => 
+          <ImageCard
+          name={image.name}
+          image={image.image}
+          key={image.id}
+        />
+        )}
+      </Wrapper>
+    );
+  }
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
