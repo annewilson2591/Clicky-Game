396141b618aa614a3cb1d4a463e296675f596398
Anne Wilson <annewilson2591@gmail.com>
2019-05-13 14:36:05 -0600

    header styling

--- clicky/src/components/Header/index.js
+++ clicky/src/components/Header/index.js
@@ -1,1 +1,13 @@
-export { default } from "./header";
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Dog Clicky Game!</h1>
+            <h2>Click on an image to earn points, but don't click any dog more than once!</h2>
+        </header>
+    );
+}
+
+export default Header;
